2dbd55f9e181d90d95c96042a7cffbfc3d410723
Aiswarya Prakasan <prakasanaiswarya@gmail.com>
2015-06-19 23:32:57 +0530

    simplify time server

--- procedures/TimeServer.py
+++ procedures/TimeServer.py
@@ -1,14 +1,5 @@
-from autobahn.asyncio.wamp import ApplicationSession
-from autobahn.asyncio.wamp import ApplicationRunner
-
-
-class TimeServer(ApplicationSession):
-    def onJoin(self, details):
-        def now(x):
-            return x
-
-        self.register(now, 'com.timeserver.now')
-
-
-runner = ApplicationRunner(url=u"ws://localhost:8080/ws", realm=u"realm1")
-runner.run(TimeServer)
+import datetime
+class TimeServer:
+    uri = 'com.timeserver.now'
+    def execute(self):
+        return datetime.datetime.now()
