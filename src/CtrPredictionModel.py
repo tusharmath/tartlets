f59084b0b252c3fed1191d6ff40513a0d0e6c81c
Aiswarya Prakasan <prakasanaiswarya@gmail.com>
2015-06-24 21:28:50 +0530

    create function createAdImpressionFeatures

--- src/CtrPredictionModel.py
+++ src/CtrPredictionModel.py
@@ -2,7 +2,6 @@ import pandas as pd
 from sklearn import linear_model
 
 import src.Utility as u
-
 import src.schemas.AdImpressionSchema as impressionSchema
 
 
@@ -39,14 +38,20 @@ class CtrPredictionModel(object):
         model = linear_model.LogisticRegression()
         impressionsDF = u.convertJsonToDataFrame(_adImpressionsJSON, impressionSchema.AdImpressionSchema)
 
-        impressionsDF[self.CONTEXT_RELEVANCE] = self.computeContextRelevanceFeature(impressionsDF)
-        impressionsDF[self.DAY_SECTION_KEY] = self.computeDaySectionFeature(impressionsDF)
-
+        features = self.createAdImpressionFeatures(_adImpressionsJSON)
         target = impressionsDF[impressionSchema.IS_CLICKED]
-        features = impressionsDF[self.FEATURE_LIST] \
-            .join(pd.get_dummies(impressionsDF[impressionSchema.AD_ID], prefix=impressionSchema.AD_ID)) \
-            .join(pd.get_dummies(impressionsDF[self.DAY_SECTION_KEY], prefix=self.DAY_SECTION_KEY))
         model.fit(features, target)
         self.model = model
         return model
+
+    def createAdImpressionFeatures(self, _adImpressionsJSON):
+        impressionsDF = u.convertJsonToDataFrame(_adImpressionsJSON, impressionSchema.AdImpressionSchema)
+
+        impressionsDF[self.CONTEXT_RELEVANCE] = self.computeContextRelevanceFeature(impressionsDF)
+        impressionsDF[self.DAY_SECTION_KEY] = self.computeDaySectionFeature(impressionsDF)
+
+        return impressionsDF[self.FEATURE_LIST] \
+            .join(pd.get_dummies(impressionsDF[impressionSchema.AD_ID], prefix=impressionSchema.AD_ID)) \
+            .join(pd.get_dummies(impressionsDF[self.DAY_SECTION_KEY], prefix=self.DAY_SECTION_KEY))
+
     
